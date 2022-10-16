32a1f01a7f6b7c2eab76cfd4593edfa29b9318d8
Keriou21 <williamandre09@gmail.com>
2022-10-16 13:09:29 +0200

    Derniere modif, dunno si l'appel au css fonctionne

--- js/charts.js
+++ js/charts.js
@@ -6,58 +6,67 @@
  * Licensed under MIT (https://coreui.io/license)
  * --------------------------------------------------------------------------
  */
+
 // random Numbers
-const random = () => Math.round(Math.random() * 100); // eslint-disable-next-line no-unused-vars
+const random = () => Math.round(Math.random() * 100)
 
-
+// eslint-disable-next-line no-unused-vars
 const lineChart = new Chart(document.getElementById('canvas-1'), {
   type: 'line',
   data: {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgba(220, 220, 220, 0.2)',
-      borderColor: 'rgba(220, 220, 220, 1)',
-      pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-      pointBorderColor: '#fff',
-      data: [random(), random(), random(), random(), random(), random(), random()]
-    }, {
-      label: 'My Second dataset',
-      backgroundColor: 'rgba(151, 187, 205, 0.2)',
-      borderColor: 'rgba(151, 187, 205, 1)',
-      pointBackgroundColor: 'rgba(151, 187, 205, 1)',
-      pointBorderColor: '#fff',
-      data: [random(), random(), random(), random(), random(), random(), random()]
-    }]
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgba(220, 220, 220, 0.2)',
+        borderColor: 'rgba(220, 220, 220, 1)',
+        pointBackgroundColor: 'rgba(220, 220, 220, 1)',
+        pointBorderColor: '#fff',
+        data: [random(), random(), random(), random(), random(), random(), random()]
+      },
+      {
+        label: 'My Second dataset',
+        backgroundColor: 'rgba(151, 187, 205, 0.2)',
+        borderColor: 'rgba(151, 187, 205, 1)',
+        pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+        pointBorderColor: '#fff',
+        data: [random(), random(), random(), random(), random(), random(), random()]
+      }
+    ]
   },
   options: {
     responsive: true
   }
-}); // eslint-disable-next-line no-unused-vars
+})
 
+// eslint-disable-next-line no-unused-vars
 const barChart = new Chart(document.getElementById('canvas-2'), {
   type: 'bar',
   data: {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      backgroundColor: 'rgba(220, 220, 220, 0.5)',
-      borderColor: 'rgba(220, 220, 220, 0.8)',
-      highlightFill: 'rgba(220, 220, 220, 0.75)',
-      highlightStroke: 'rgba(220, 220, 220, 1)',
-      data: [random(), random(), random(), random(), random(), random(), random()]
-    }, {
-      backgroundColor: 'rgba(151, 187, 205, 0.5)',
-      borderColor: 'rgba(151, 187, 205, 0.8)',
-      highlightFill: 'rgba(151, 187, 205, 0.75)',
-      highlightStroke: 'rgba(151, 187, 205, 1)',
-      data: [random(), random(), random(), random(), random(), random(), random()]
-    }]
+    datasets: [
+      {
+        backgroundColor: 'rgba(220, 220, 220, 0.5)',
+        borderColor: 'rgba(220, 220, 220, 0.8)',
+        highlightFill: 'rgba(220, 220, 220, 0.75)',
+        highlightStroke: 'rgba(220, 220, 220, 1)',
+        data: [random(), random(), random(), random(), random(), random(), random()]
+      },
+      {
+        backgroundColor: 'rgba(151, 187, 205, 0.5)',
+        borderColor: 'rgba(151, 187, 205, 0.8)',
+        highlightFill: 'rgba(151, 187, 205, 0.75)',
+        highlightStroke: 'rgba(151, 187, 205, 1)',
+        data: [random(), random(), random(), random(), random(), random(), random()]
+      }
+    ]
   },
   options: {
     responsive: true
   }
-}); // eslint-disable-next-line no-unused-vars
+})
 
+// eslint-disable-next-line no-unused-vars
 const doughnutChart = new Chart(document.getElementById('canvas-3'), {
   type: 'doughnut',
   data: {
@@ -71,37 +80,42 @@ const doughnutChart = new Chart(document.getElementById('canvas-3'), {
   options: {
     responsive: true
   }
-}); // eslint-disable-next-line no-unused-vars
+})
 
+// eslint-disable-next-line no-unused-vars
 const radarChart = new Chart(document.getElementById('canvas-4'), {
   type: 'radar',
   data: {
     labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgba(220, 220, 220, 0.2)',
-      borderColor: 'rgba(220, 220, 220, 1)',
-      pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-      pointBorderColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(220, 220, 220, 1)',
-      data: [65, 59, 90, 81, 56, 55, 40]
-    }, {
-      label: 'My Second dataset',
-      backgroundColor: 'rgba(151, 187, 205, 0.2)',
-      borderColor: 'rgba(151, 187, 205, 1)',
-      pointBackgroundColor: 'rgba(151, 187, 205, 1)',
-      pointBorderColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(151, 187, 205, 1)',
-      data: [28, 48, 40, 19, 96, 27, 100]
-    }]
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgba(220, 220, 220, 0.2)',
+        borderColor: 'rgba(220, 220, 220, 1)',
+        pointBackgroundColor: 'rgba(220, 220, 220, 1)',
+        pointBorderColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220, 220, 220, 1)',
+        data: [65, 59, 90, 81, 56, 55, 40]
+      },
+      {
+        label: 'My Second dataset',
+        backgroundColor: 'rgba(151, 187, 205, 0.2)',
+        borderColor: 'rgba(151, 187, 205, 1)',
+        pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+        pointBorderColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(151, 187, 205, 1)',
+        data: [28, 48, 40, 19, 96, 27, 100]
+      }
+    ]
   },
   options: {
     responsive: true
   }
-}); // eslint-disable-next-line no-unused-vars
+})
 
+// eslint-disable-next-line no-unused-vars
 const pieChart = new Chart(document.getElementById('canvas-5'), {
   type: 'pie',
   data: {
@@ -115,8 +129,9 @@ const pieChart = new Chart(document.getElementById('canvas-5'), {
   options: {
     responsive: true
   }
-}); // eslint-disable-next-line no-unused-vars
+})
 
+// eslint-disable-next-line no-unused-vars
 const polarAreaChart = new Chart(document.getElementById('canvas-6'), {
   type: 'polarArea',
   data: {
@@ -129,5 +144,40 @@ const polarAreaChart = new Chart(document.getElementById('canvas-6'), {
   options: {
     responsive: true
   }
-});
-//# sourceMappingURL=charts.js.map
+})
+
+
+// New chart
+
+const linechart2 = new Chart(document.getElementById('lineChart'), {
+  type: 'line',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+      label: "My First dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(105, 0, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(200, 99, 132, .7)',
+      ],
+      borderWidth: 2
+    },
+    {
+      label: "My Second dataset",
+      data: [28, 48, 40, 19, 86, 27, 90],
+      backgroundColor: [
+        'rgba(0, 137, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(0, 10, 130, .7)',
+      ],
+      borderWidth: 2
+    }
+    ]
+  },
+  options: {
+    responsive: true
+  }
+})
